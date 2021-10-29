1611f4fae3e0a2461004c4f4cfaf3b76425b7cfe
normanvnfc <norman@venuerific.com>
2021-10-29 22:15:16 +0700

    Create toggle function for marker info

--- app/js/_global.js
+++ app/js/_global.js
@@ -1,12 +1,12 @@
 // Global
 app.global = {
-    init: function(){ // Load all global functions here
-        console.log("load global functions");
-        app.global.loadHeader();
-    },
-    loadHeader: function(){ // Some specific function
-        console.log("loadHeader()");
-    }
+	init: function () { // Load all global functions here
+		console.log("load global functions");
+		app.global.loadHeader();
+	},
+	loadHeader: function () { // Some specific function
+		console.log("loadHeader()");
+	}
 }
 
 // Run the global stuff
--- app/js/script.js
+++ app/js/script.js
@@ -2,3 +2,9 @@ console.log("Load script.js");
 
 // Instantiating the global app object
 var app = {};
+
+const toggleIcon = document.querySelector(".action--toggle");
+const markerInfoCard = document.querySelector(".card--bottom");
+toggleIcon.addEventListener("click", ()=>{
+  markerInfoCard.classList.toggle("active");
+});
