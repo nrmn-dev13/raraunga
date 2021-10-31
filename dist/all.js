0acfac269fd882eee9c86f9b78ab6c4f5dab1a4a
normanvnfc <norman@venuerific.com>
2021-10-31 19:34:19 +0700

    Moving img to dist

---
+++ dist/all.js
@@ -0,0 +1,2 @@
+console.log("Load script.js");var app={};const toggleIcon=document.querySelector(".action--toggle"),markerInfoCard=document.querySelector(".card--bottom");toggleIcon.addEventListener("click",(()=>{markerInfoCard.classList.toggle("active")})),app.global={init:function(){console.log("load global functions"),app.global.loadHeader()},loadHeader:function(){console.log("loadHeader()")}},app.global.init();
+//# sourceMappingURL=all.js.map
